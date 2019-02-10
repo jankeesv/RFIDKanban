210bce9054b6cb02455843a7cf1a2c8d739ce187
Jan Kees Velthoven <info@velthovenconsultancy.nl>
2019-02-10 13:32:02 +0100

    Added edit functionality for tags and registrations

---
+++ KanbanStatsServer/WebApplication/Controllers/RFIDRegistrationsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class RFIDRegistrationsController : Controller
+    {
+        private RFIDKanbanEntities db = new RFIDKanbanEntities();
+
+        // GET: RFIDRegistrations
+        public ActionResult Index()
+        {
+            return View(db.RFIDRegistrations.ToList().OrderBy(r => r.RegistrationDateTime));
+        }
+
+        // GET: RFIDRegistrations/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RFIDRegistrations rFIDRegistrations = db.RFIDRegistrations.Find(id);
+            if (rFIDRegistrations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rFIDRegistrations);
+        }
+
+        // GET: RFIDRegistrations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RFIDRegistrations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Station,RegistrationDateTime,ExerciseName,TagName")] RFIDRegistrations rFIDRegistrations)
+        {
+            if (ModelState.IsValid)
+            {
+                rFIDRegistrations.ID = Guid.NewGuid();
+                db.RFIDRegistrations.Add(rFIDRegistrations);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rFIDRegistrations);
+        }
+
+        // GET: RFIDRegistrations/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RFIDRegistrations rFIDRegistrations = db.RFIDRegistrations.Find(id);
+            if (rFIDRegistrations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rFIDRegistrations);
+        }
+
+        // POST: RFIDRegistrations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Station,RegistrationDateTime,ExerciseName,TagName")] RFIDRegistrations rFIDRegistrations)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rFIDRegistrations).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rFIDRegistrations);
+        }
+
+        // GET: RFIDRegistrations/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RFIDRegistrations rFIDRegistrations = db.RFIDRegistrations.Find(id);
+            if (rFIDRegistrations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rFIDRegistrations);
+        }
+
+        // POST: RFIDRegistrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            RFIDRegistrations rFIDRegistrations = db.RFIDRegistrations.Find(id);
+            db.RFIDRegistrations.Remove(rFIDRegistrations);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ KanbanStatsServer/WebApplication/Controllers/RFIDTagsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class RFIDTagsController : Controller
+    {
+        private RFIDKanbanEntities db = new RFIDKanbanEntities();
+
+        // GET: RFIDTags
+        public ActionResult Index()
+        {
+            return View(db.RFIDTag.ToList().OrderBy(t => t.Name));
+        }
+
+        // GET: RFIDTags/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RFIDTag rFIDTag = db.RFIDTag.Find(id);
+            if (rFIDTag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rFIDTag);
+        }
+
+        // GET: RFIDTags/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RFIDTags/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,RFIDUID,Name,Type")] RFIDTag rFIDTag)
+        {
+            if (ModelState.IsValid)
+            {
+                rFIDTag.ID = Guid.NewGuid();
+                db.RFIDTag.Add(rFIDTag);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rFIDTag);
+        }
+
+        // GET: RFIDTags/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RFIDTag rFIDTag = db.RFIDTag.Find(id);
+            if (rFIDTag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rFIDTag);
+        }
+
+        // POST: RFIDTags/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,RFIDUID,Name,Type")] RFIDTag rFIDTag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rFIDTag).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rFIDTag);
+        }
+
+        // GET: RFIDTags/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RFIDTag rFIDTag = db.RFIDTag.Find(id);
+            if (rFIDTag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rFIDTag);
+        }
+
+        // POST: RFIDTags/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            RFIDTag rFIDTag = db.RFIDTag.Find(id);
+            db.RFIDTag.Remove(rFIDTag);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
